bbccc94132e1fca0ee04c5a584320437f1b5df08
peta-hub <pmonroylara@gmail.com>
2021-02-10 08:14:05 -0600

    credenciales de db de heroku añadidas

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = ['djangoblogpildoras.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd25ongkgdl9u4d',
+        'User': 'knanctqxddzqej',
+        'Password': 'b6c1b3fdf8c96b2f850462ca426c073be96a729dc5f39714072bc0b7e52def9d',
+        'HOST': 'ec2-100-24-139-146.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
 
